d495c92a459d54891315c8e7f9136e6319795b55
Colm Murphy <colmmurphy016@gmail.com>
2022-02-24 11:39:31 +0000

    create simple login/signup forms

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
-from flask import Flask, render_template, request, redirect
-from werkzeug.security import generate_password_hash
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 from database import get_db, close_db
-from forms import HomePageForm, SignUpForm
+from forms import HomePageForm, SignUpForm, LoginForm
 from datetime import *
 
 app = Flask(__name__)
 app.teardown_appcontext(close_db)
 app.config["SECRET_KEY"] = "super-secret-key"
 
+
 @app.route("/")
 def root():
     # check if user already logged in
     # if not, redirect them to login/signup page
     # else, show them the homepage
-    return redirect("http://127.0.0.1:5000/home", code=302)
+    return redirect(url_for("home"), code=302)
 
-@app.route("/home",  methods=["GET", "POST"])
+
+@app.route("/home", methods=["GET", "POST"])
 def home():
     db = get_db()
     form = HomePageForm()
@@ -29,19 +31,20 @@ def home():
         query += "datetime('now', '-7 days')"
     elif recent == 'this month':
         query += "datetime('now', '-31 days')"
-    else:   # elif recent == 'all time':
+    else:  # elif recent == 'all time':
         query += "0"
     query += " AND submission_time <= datetime('now') "
     if sort_by == "most recent":
         query += "ORDER BY submission_time DESC"
-    else:   # elif sort_by == "most popular"
-        query += "ORDER BY likes DESC"     # i haven't implemented likes yet lol
+    else:  # elif sort_by == "most popular"
+        query += "ORDER BY likes DESC"  # i haven't implemented likes yet lol
     query += ";"
 
     posts = db.execute(query).fetchall()
     posts_formatted = []
     for post in posts:
-        likes = db.execute("SELECT COUNT(*) AS 'likes' FROM likes WHERE post_id = ?;", (post["id"],)).fetchone()["likes"]
+        likes = db.execute("SELECT COUNT(*) AS 'likes' FROM likes WHERE post_id = ?;", (post["id"],)).fetchone()[
+            "likes"]
         posts_formatted.append(
             {
                 "message": post["message"],
@@ -52,15 +55,48 @@ def home():
         )
     return render_template("home.html", form=form, posts=posts_formatted)
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        db = get_db()
+
+        username_exists_in_db = db.execute("SELECT * FROM users WHERE username = ?;",
+                                           (username,)).fetchone().keys() != 0
+        if not username_exists_in_db:
+            form.username.errors.append("There is no user called %s" % username)
+            return render_template("login.html", form=form, message="")
+        password_in_db = db.execute("SELECT password_hash FROM users WHERE username = ?;",
+                                    (username,)).fetchone()["password_hash"]
+        password_correct = check_password_hash(password_in_db, password)
+        if not password_correct:
+            form.password.errors.append("Incorrect password :(")
+            return render_template("login.html", form=form, message=f"You are now logged in as {username}")
+
+        return render_template("login.html", form=form, message=f"You are now logged in as {username}")
+    return render_template("login.html", form=form)
+
+
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     form = SignUpForm()
-    username = ""
-    password = ""
     if form.validate_on_submit():
         username = form.username.data
         password_hash = generate_password_hash(form.password.data)
         db = get_db()
+        # validations
+        users_with_same_name = db.execute("""
+        SELECT COUNT(*) AS 'users_with_same_name'
+        FROM users 
+        WHERE username = ?;""",
+                                          (username,)).fetchone()["users_with_same_name"]
+        if users_with_same_name > 0:
+            form.username.errors.append(username + " is already taken")
+            return render_template("signup.html", form=form)
         db.execute("INSERT INTO users (username, password_hash) VALUES (?, ?)", (username, password_hash))
         db.commit()
+        return redirect(url_for("home"), code=302)
     return render_template("signup.html", form=form)
--- forms.py
+++ forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, SelectField, StringField, SubmitField
 from wtforms.validators import EqualTo, InputRequired, Length
 
+
 class HomePageForm(FlaskForm):
     sort_by = SelectField(label="Show me the ", choices=["most recent", "most popular"],
                           default="most recent")
@@ -9,11 +10,20 @@ class HomePageForm(FlaskForm):
                          default="this week")
     submit = SubmitField("Submit")
 
+
 class SignUpForm(FlaskForm):
     username = StringField("Username:",
-                            validators=[InputRequired(), Length(4, 20)])
+                           validators=[InputRequired(), Length(4, 20)])
     password = PasswordField("Password:",
-                           validators=[InputRequired(), Length(min=6)])
+                             validators=[InputRequired(), Length(min=6)])
     password2 = PasswordField("Re-enter your password",
-                            validators=[EqualTo("password")])
+                              validators=[EqualTo("password")])
+    submit = SubmitField()
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username:",
+                           validators=[InputRequired(), Length(4, 20)])
+    password = PasswordField("Password",
+                             validators=[InputRequired(), Length(min=6)])
     submit = SubmitField()
